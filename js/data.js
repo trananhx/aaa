0224bae8e61c58e932493204cd024d64be276eab
Anh <trananh11929@gmail.com>
2021-05-20 20:42:44 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,68 @@
+    var testdata = {
+    "data": [
+    {
+    "name": "Tiger Nixon",
+    "salary": "$320,800",
+    "start_date": "2011/04/25",
+    "office": "Edinburgh",
+    "extn": "5421"
+    },
+    {
+    "name": "Garrett Winters",
+   "salary": "$170,750",
+    "start_date": "2011/07/25",
+    "office": "Tokyo",
+    "extn": "8422"
+    },
+    {
+    "name": "Ashton Cox",
+    "salary": "$86,000",
+    "start_date": "2009/01/12",
+    "office": "San Francisco",
+    "extn": "1562"
+    },
+    {
+    "name": "Cedric Kelly",
+    "salary": "$433,060",
+    "start_date": "2012/03/29",
+    "office": "Edinburgh",
+    "extn": "6224"
+    },
+    {
+    "name": "Airi Satou",
+    "salary": "$162,700",
+    "start_date": "2008/11/28",
+    "office": "Tokyo",
+    "extn": "5407"
+    },
+    {
+    "name": "Brielle Williamson",
+    "salary": "$372,000",
+    "start_date": "2012/12/02",
+    "office": "New York",
+    "extn": "4804"
+    },
+    {
+    "name": "Herrod Chandler",
+    "salary": "$137,500",
+    "start_date": "2012/08/06",
+    "office": "San Francisco",
+    "extn": "9608"
+    },
+    {
+    "name": "Rhona Davidson",
+    "salary": "$327,900",
+    "start_date": "2010/10/14",
+    "office": "Tokyo",
+    "extn": "6200"
+    },
+    {
+    "name": "Colleen Hurst",
+    "salary": "$205,500",
+    "start_date": "2009/09/15",
+    "office": "San Francisco",
+    "extn": "2360"
+    },
+   
+    ]
+    };
